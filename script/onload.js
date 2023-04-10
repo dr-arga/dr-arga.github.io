a7de6ad5ab69d6a9268438f871a0aef3161c7c6e
Komite PPI RSPA <praktekdrarga@gmail.com>
2023-04-10 09:47:05 +0700

    1

--- script/onload.js
+++ script/onload.js
@@ -1,7 +1,7 @@
 function onload(){
     includeHTML()
     setTimeout(HideDiv, 1000)
-    developing(true)
+    developing(false)
 }
 function HideDiv(){
     var j = document.querySelectorAll("#hidden-div > div")
